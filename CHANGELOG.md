58e7fb272642040ecbaedb16a3f138fe6ee684d6
Sem Antonyan <sem.antony.dev@gmail.com>
2025-04-28 17:58:43 +0400

    tets

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# desing-system
+
+## 0.0.1-next.0
+
+### Patch Changes
+
+- feat: добавлен базовый компонент Button
+
+- test
+
+- test: this is test
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import {defineConfig} from 'vite'
+import {dirname, resolve} from 'node:path'
+import {fileURLToPath} from 'node:url'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vite.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'lib/main.js'),
+            name: 'ui-core',
+            // the proper extensions will be added
+            fileName: 'ui-core',
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['react', 'react-dom', 'react/jsx-runtime'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    react: 'React',
+                    "react-dom": 'ReactDOM',
+                    "react/jsx-runtime": 'jsxRuntime',
+                },
+            },
+        },
+    },
+})
