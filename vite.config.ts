3a40e938d758e89fe99a5f9901dd348f642e18a0
Sem Antonyan <sem.antony.dev@gmail.com>
2025-04-28 22:01:17 +0400

    update changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import {dirname, resolve} from 'node:path'
 import {fileURLToPath} from 'node:url'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from "vite-tsconfig-paths";
+import {dependencies, devDependencies} from "./package.json"
 
 // https://vite.dev/config/
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -19,7 +20,7 @@ export default defineConfig({
         rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
             // into your library
-            external: ['react', 'react-dom', 'react/jsx-runtime'],
+            external: [...Object.keys(dependencies), ...Object.keys(devDependencies), 'react', 'react-dom', 'react/jsx-runtime'],
             output: {
                 // Provide global variables to use in the UMD build
                 // for externalized deps
